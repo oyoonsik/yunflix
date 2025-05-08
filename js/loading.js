e2f36a8b1ff4070d8c54b9b23f5a8eac711993c1
oyoonsik <38741838+oyoonsik@users.noreply.github.com>
2025-05-08 13:56:59 +0900

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,20 @@
+// intro.js
+document.addEventListener('DOMContentLoaded', () => {
+    const sound = document.getElementById('yunflix-sound');
+    
+    // 두둥 사운드 재생
+    sound.currentTime = 0;
+    sound.play();
+
+    // 2.8초 후 index.html로 이동
+    setTimeout(() => {
+        window.location.href = 'index.html';
+    }, 2800); // 2800ms == 2.8초
+
+    // 새로고침 시 애니메이션 다시 시작
+    window.addEventListener('pageshow', (event) => {
+        if (event.persisted) {
+            window.location.reload();
+        }
+    });
+});
